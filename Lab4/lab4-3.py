fcc090d18b4bdd6c7acdf079f5d41ff0e1a45aca
hoswened <romka221175@gmail.com>
2024-12-20 11:17:46 +0300

    Добавлен новый файл

---
+++ Lab4/lab4-3.py
@@ -0,0 +1,5 @@
+import my_package
+
+print(my_package.vremya.now())  # эта функция пишет текущую дату и время
+print(my_package.math_ops.sum(15, 5))  # суммирует числа
+print(my_package.math_ops.subt(15, 5))  # вычитает числа
